b4d5306d38b01cc1c7786e307776d2af8d7bfb88
Fernanda Marques Anastácio <marquesfeh74@gmail.com>
2024-10-24 14:35:34 -0300

    Desafio02C#

--- DesafioLaco02/Program.cs
+++ DesafioLaco02/Program.cs
@@ -1,3 +1,33 @@
-﻿
-int qntdh = 0;
-int qntdm = 0;
+﻿int homens = 0;
+int mulheres = 0;
+int idadeHomem = 0;
+int idadeMulher = 0;
+
+for(int i = 1; i <=4; i++)
+{
+    Console.WriteLine($"Você é homem ou mulher? m/f");
+    string genero = Console.ReadLine();
+    
+    Console.WriteLine($"Qual sua idade?");
+    int idade = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (genero == "m")
+    {
+        homens ++;
+        idadeHomem+=idade;
+    }
+    else 
+    {
+        mulheres ++;
+        idadeMulher+=idade;
+    }
+}
+float mediaHomem = (idadeHomem / homens);
+float mediaMulher = (idadeMulher / mulheres);
+
+
+Console.WriteLine($"Total de homens {homens}");
+Console.WriteLine($"Total de Mulheres {mulheres}");
+Console.WriteLine($"Media de idade dos homens {mediaHomem}");
+Console.WriteLine($"Media de idade das mulheres {mediaMulher}");
