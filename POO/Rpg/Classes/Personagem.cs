ea3a66244f74adf9fbdfec60e4bdeb4875eb7947
Fernanda Marques Anastácio <marquesfeh74@gmail.com>
2024-11-07 10:32:57 -0300

    Aula 07/11

---
+++ POO/Rpg/Classes/Personagem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.Classes
+{
+    public class Personagem
+    {
+        public string Nome;
+        public int idade;
+        public string aramdura;
+        public string ia;
+
+        public void 
+    }
+}
